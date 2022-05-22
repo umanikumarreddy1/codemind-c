7cfb5ee27df1549e38da79e0ed76504b8e4d281e
U MANI KUMAR REDDY <98727613+umanikumarreddy1@users.noreply.github.com>
2022-05-22 13:57:24 +0530

    #include<stdio.h> int main() {     long int a;     scanf("%ld",&a);     if (a>=1000000000 && a<=9999999999)     {         if (a%100000000!=0)         {             printf("Valid");         }         else         {             printf("Invalid");         }     }     else     {         printf("Invalid");     }     return 0; }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    long int a;
+    scanf("%ld",&a);
+    if (a>=1000000000 && a<=9999999999)
+    {
+        if (a%100000000!=0)
+        {
+            printf("Valid");
+        }
+        else
+        {
+            printf("Invalid");
+        }
+    }
+    else
+    {
+        printf("Invalid");
+    }
+    return 0;
+}
